59942d9166c8904113f3c56153d55de7764f1c52
Edward <edward@bwired.ca>
2022-02-04 02:17:46 -0500

    WebApi unused reference deleted - add a NUnit project for Unit Test

---
+++ UnitTest/UnitTestApi.cs
@@ -0,0 +1,43 @@
+using Business_Logic_Layer.Service;
+using Data_Access_Layer;
+using Entity;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Web_API.Controllers;
+using Data_Access_Layer.Repository;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        EmployeeController _controller;
+        IEmployeeService _service;
+        IEmployee _employee;
+
+        public Tests()
+        {
+            _employee = new Employee();
+            _service = new EmployeeService(_employee); 
+            _controller = new EmployeeController(_service);
+           
+        }
+
+        [Test]
+        public void GetAllEmployee()
+        {
+            
+            //https://medium.com/c-sharp-progarmming/unit-testing-in-asp-net-core-web-api-b2e6f7bdb860
+
+            var controller = new EmployeeController(_service);
+            List<EmployeeEntity> result2 = new List<EmployeeEntity>();
+            var result = controller.GetAllEmployee();
+            //Assert.IsNotNull(result);
+            //Assert.AreEqual(false, result.IsCompletedSuccessfully);
+            //Assert.AreEqual(true, result.IsCompleted);
+             Assert.AreEqual(false, result.IsFaulted);
+            
+
+        }
+    }
+}
